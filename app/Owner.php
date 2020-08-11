eca5b4607038fca049f7954324e7225a2415020e
deanssmart <deanssmart@gmail.com>
2020-08-11 15:16:21 +0100

    static banans method added with working unit tests

--- app/Owner.php
+++ app/Owner.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //
+    public static function haveWeBananas($number)
+    {
+        if ($number <= 0) {
+            return "No we have no bananas";
+        } else if ($number === 1) {
+            return "Yes we have {$number} banana";
+        } else {
+            return "Yes we have {$number} bananas";
+        }
+    }
 }
---
+++ tests/Unit/BananaTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Owner;
+
+class BananaTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testBananas()
+    {
+        $this->assertSame(Owner::haveWeBananas(0), "No we have no bananas");
+        $this->assertSame(Owner::haveWeBananas(2), "Yes we have 2 bananas");
+        
+        $this->assertSame(Owner::haveWeBananas(1), "Yes we have 1 banana");
+        $this->assertSame(Owner::haveWeBananas(-12), "No we have no bananas");
+    }
+}
