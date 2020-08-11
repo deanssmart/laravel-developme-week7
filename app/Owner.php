70aafe4fcca727227b2df63cfffebc014046e73b
deanssmart <deanssmart@gmail.com>
2020-08-12 00:33:39 +0100

    owner class methods for fullname fulladdress and formattedphonenumber added

--- app/Owner.php
+++ app/Owner.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Owner extends Model
 {
-    public static function haveWeBananas($number)
+    public static function haveWeBananas(int $number) : string
     {
         if ($number <= 0) {
             return "No we have no bananas";
@@ -15,5 +16,30 @@ class Owner extends Model
         } else {
             return "Yes we have {$number} bananas";
         }
+    }
+
+    public function fullName() : string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+
+    public function fullAddress() : string
+    {
+        if($this->address_2 !== null){
+            $fullAddress = collect([$this->address_1, $this->address_2, $this->town, $this->postcode]);
+            return $fullAddress->join("\n");
+        } else {
+            $fullAddress = collect([$this->address_1, $this->town, $this->postcode]);
+            return $fullAddress->join("\n");
+        }
+    }
+
+    public function formattedPhoneNumber()
+    {
+        $formattedNumber = sprintf("%s %s %s",
+                                   substr($this->telephone, 0, 4),
+                                   substr($this->telephone, 4, 3),
+                                   substr($this->telephone, 7, 4));
+        return $formattedNumber;
     }
 }
