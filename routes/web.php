6bfdc1586d621ddf7a6e1488dd5e973861101ad4
deanssmart <deanssmart@gmail.com>
2020-10-21 20:52:30 +0100

    login removed

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', "Home@index");
 Route::get('/home', "Home@index");
 
 Route::group(["prefix" => "owners"], function(){    
-    Route::group(["middleware" => "auth"], function() {
+    // Route::group(["middleware" => "auth"], function() {
         Route::get('create', "Owners@create");
         Route::post('create', "Owners@createPost");
         Route::post('delete/{owner}', "Owners@destroy");
@@ -26,27 +26,27 @@ Route::group(["prefix" => "owners"], function(){
             Route::get('{owner}', "Owners@edit");   
             Route::post('{owner}', "Owners@editPost");
         }); 
-    });
+    // });
     Route::get('index', "Owners@index");
     Route::get('search', "Owners@search");
     Route::get('{owner}', "Owners@show");
 });
 
 Route::group(["prefix" => "animals"], function(){
-    Route::group(["middleware" => "auth"], function() {
+    // Route::group(["middleware" => "auth"], function() {
         Route::post('delete/{animal}', "Animals@destroy");
         Route::group(["prefix" => "edit"], function(){
             Route::get('{animal}', "Animals@edit");   
             Route::post('{animal}', "Animals@editPost");
         });
 
-    });
+    // });
 });
 
 Route::group(["prefix" => "treatments"], function(){
-    Route::group(["middleware" => "auth"], function() {
+    // Route::group(["middleware" => "auth"], function() {
         Route::post('delete/{treatment}', "Treatments@destroy");
-    });
+    // });
     Route::get('index', "Treatments@index");
     Route::get('{treatment}', "Treatments@show");
 });
